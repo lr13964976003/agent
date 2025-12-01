0801652a40062cab5eeafe895dc27f521556be32
lr <73323152+lr13964976003@users.noreply.github.com>
2025-12-01 10:45:44 +0800

    Update method.py

--- paper_to_dag/method.py
+++ paper_to_dag/method.py
@@ -31,74 +31,7 @@ def main():
     MAX_ITER = 2
 
     variant = {
-            "check_topic": {
-                "slug": "check_topic",
-                "version": 10,
-                "inputs": {
-                    "paper_path": "../papers/EP/paper.md",
-                    "score_path": "../knowledges/llm_parallelism_classification_schema.json"
-                },
-                "tools": [
-                FileReadTool()
-                ]
-            },
-            "read_paper": {
-                "slug": "chain_read_paper",
-                "version": 10,
-                "inputs": {
-                    "paper_path": "../papers/EP/paper.md",
-                    "knowledge_path": "../knowledges/llm_parallel_strategies.md",
-                    "save_path": f"../outputs/{submission_dir}"
-                    },
-                "tools": [
-                    FileReadTool(),
-                    FileWriterTool(),
-                    AppendFileTool(),
-                    CommandTool()
-                    ]
-                },
-            "check_paper": {
-                "slug" : "chain_check_paper",
-                "version" : 8,
-                "inputs": {
-                    "origin_paper_path" : "../papers/EP/paper.md"
-                    },
-                "tools": [
-                    FileReadTool(),
-                    FileWriterTool(),
-                    AppendFileTool(),
-                    CommandTool(),
-                    SearchFileTool()
-                    ]
-                },
-            "generate_dag": {
-                "slug": "chain_generate_dag",
-                "version": 15,
-                "inputs": {
-                    "knowledge_path": "../knowledges/llm_parallel_strategies.md",
-                    "save_path": f"../outputs/{submission_dir}"
-                    },
-                "tools": [
-                    FileReadTool(),
-                    FileWriterTool(),
-                    CommandTool(),
-		            PythonTool(),
-                    ExtractEdgeFromDAGTool()
-                    ]
-                },
-             "check_dag": {
-                 "slug": "chain_check_dag",
-                 "version": 3,
-                 "inputs": {
-                     "save_path": f"../outputs/{submission_dir}"
-                     },
-                 "tools": [
-                     FileWriterTool(),
-                     CommandTool(),
-                     ExtractEdgeFromDAGTool()
-                     ]
-                 },
-             "generate_method": {
+		    "generate_method": {
                  "slug": "chain_generate_method",
                  "version": 1,
                  "inputs": {
@@ -128,38 +61,38 @@ def main():
                      FileWriterTool()
                      ]
                  },
-		     "performance": {
-                 "slug": "chain_performance",
-                 "version": 17,
+            "generate_dag": {
+                "slug": "chain_generate_dag",
+                "version": 15,
+                "inputs": {
+                    "knowledge_path": "../knowledges/llm_parallel_strategies.md",
+                    "save_path": f"../outputs/{submission_dir}"
+                    },
+                "tools": [
+                    FileReadTool(),
+                    FileWriterTool(),
+                    CommandTool(),
+		            PythonTool(),
+                    ExtractEdgeFromDAGTool()
+                    ]
+                },
+             "check_dag": {
+                 "slug": "chain_check_dag",
+                 "version": 3,
                  "inputs": {
                      "save_path": f"../outputs/{submission_dir}"
                      },
                  "tools": [
-                     ExtractEdgeFromDAGTool(),
-                     FileReadTool(),
+                     FileWriterTool(),
                      CommandTool(),
-                     FileWriterTool()
+                     ExtractEdgeFromDAGTool()
                      ]
-                 },
-              "iter_generate_dag": {
-                  "slug": "iter_generate_dag",
-                  "version": 17,
-                  "inputs": {
-                      "knowledge_path": "../knowledges/llm_parallel_strategies.md",
-                      "save_path": f"../outputs/{submission_dir}"
-                  },
-                  "tools": [
-                  ExtractEdgeFromDAGTool(),
-                  FileReadTool(),
-                  CommandTool(),
-                  FileWriterTool()
-                  ]
-              }
+                 }
             }
     agents = []
     tasks = []
     i = 0
-    expected_outputs = ["Check Result", "The file path of concise paper and deployment configuration", "Check Result", "The path of graphviz code describing the DAG", "Check Result", "The performance of DAG", "The path of graphviz code describing the DAG"]
+    expected_outputs = ["The file path of the deployment method", "Check Result", "The path of graphviz code describing the DAG", "Check Result"]
     for k in variant.keys():
         prompt = fetch_prompt_local(variant[k]["slug"], variant[k]["inputs"])
         tools = variant[k]["tools"]
@@ -171,11 +104,9 @@ def main():
     #if "failed" in check_result.lower():
     #    return "The paper is not relevant to the topic"
 
-    # paper_loop = ReviewLoop(worker=agents[1], reviewer=agents[2], work_task=tasks[1], review_task=tasks[2])
-    # paper_result = paper_loop.run()
-    method_loop = ReviewLoop(worker=agents[5], reviewer=agents[6], work_task=tasks[5], review_task=tasks[6])
+    method_loop = ReviewLoop(worker=agents[0], reviewer=agents[1], work_task=tasks[0], review_task=tasks[1])
     method_result = method_loop.run()
-    dag_loop = ReviewLoop(worker=agents[3], reviewer=agents[4], work_task=tasks[3], review_task=tasks[4], inputs=paper_result)
+    dag_loop = ReviewLoop(worker=agents[2], reviewer=agents[3], work_task=tasks[2], review_task=tasks[3], inputs=environment_result)
     dag_result = dag_loop.run()
     
     return
