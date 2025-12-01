f2d414bf1118e7a496e27d533f69492b4aa803ea
lr <73323152+lr13964976003@users.noreply.github.com>
2025-12-01 10:37:33 +0800

    Update method.py

--- paper_to_dag/method.py
+++ paper_to_dag/method.py
@@ -102,8 +102,8 @@ def main():
                  "slug": "chain_generate_method",
                  "version": 1,
                  "inputs": {
-					           "environment_path": "../environment/EP/deployment.md",
-					           "knowledge_path": "../knowledges/llm_parallel_strategies.md",
+					 "environment_path": "../environment/EP/deployment.md",
+					 "knowledge_path": "../knowledges/llm_parallel_strategies.md",
                      "save_path": f"../outputs/{submission_dir}"
                      },
                  "tools": [
@@ -173,7 +173,7 @@ def main():
 
     # paper_loop = ReviewLoop(worker=agents[1], reviewer=agents[2], work_task=tasks[1], review_task=tasks[2])
     # paper_result = paper_loop.run()
-	  method_loop = ReviewLoop(worker=agents[5], reviewer=agents[6], work_task=tasks[5], review_task=tasks[6])
+    method_loop = ReviewLoop(worker=agents[5], reviewer=agents[6], work_task=tasks[5], review_task=tasks[6])
     method_result = method_loop.run()
     dag_loop = ReviewLoop(worker=agents[3], reviewer=agents[4], work_task=tasks[3], review_task=tasks[4], inputs=paper_result)
     dag_result = dag_loop.run()
