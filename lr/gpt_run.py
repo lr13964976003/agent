343a855c0f454d4af5f1852dd3b239122b4b0b95
lr <73323152+lr13964976003@users.noreply.github.com>
2025-12-26 15:44:49 +0800

    Update gpt_run.py

--- lr/gpt_run.py
+++ lr/gpt_run.py
@@ -1,4 +1,4 @@
-# main.py
+ls# main.py
 import os
 import time
 import agenta as ag
@@ -8,7 +8,7 @@ from crewai_tools import ScrapeWebsiteTool, FileReadTool, FileWriterTool
 from langchain_openai import ChatOpenAI
 import re
 from tools import *
-from build_agents import *
+from gpt_build_agent import *
 from datetime import datetime 
 from opentelemetry import trace
 
@@ -32,8 +32,8 @@ def main():
     MAX_ITER = 2
 
     variant = {
-		    "generate_method": {
-                "slug": "generate_method",
+		    "gpt": {
+                "slug": "gpt_prompt",
                 "version": 1,
                 "inputs": {
 					"task_path": "./inputs/task.md",
@@ -46,48 +46,6 @@ def main():
                      FileWriterTool()
                      ]
                  },
-		     "check_method": {
-                 "slug": "performance_evaluation",
-                 "version": 1,
-                 "inputs": {
-					 "task_path": "./inputs/task.md",
-                     "save_path": f"./outputs/{submission_dir}"
-                     },
-                 "tools": [
-                     FileReadTool(),
-					 PythonTool(),
-                     CommandTool(),
-                     FileWriterTool()
-                     ]
-                 },
-            "generate_dag": {
-                "slug": "generate_dag",
-                "version": 1,
-                "inputs": {
-                    "save_path": f"./outputs/{submission_dir}"
-                    },
-                "tools": [
-                    FileReadTool(),
-                    FileWriterTool(),
-                    CommandTool(),
-		            PythonTool(),
-                    ExtractEdgeFromDAGTool()
-                    ]
-                },
-             "check_dag": {
-                 "slug": "check_dag",
-                 "version": 1,
-                 "inputs": {
-                     "save_path": f"./outputs/{submission_dir}"
-                     },
-                 "tools": [
-                    FileReadTool(),
-                    FileWriterTool(),
-                    CommandTool(),
-		            PythonTool(),
-                    ExtractEdgeFromDAGTool()
-                     ]
-                 }
             }
     prompts = []
     tools = []
@@ -95,21 +53,9 @@ def main():
     for k in variant.keys():
         prompts.append(fetch_prompt_local(variant[k]["slug"], variant[k]["inputs"]))
         tools.append(variant[k]["tools"])
-	# Generate_Method_Agent
-    GMA = Researcher("openai/Kimi-K2",tools[0])
-    GMT = build_task(prompts[0], expected_outputs[0], GMA)
-
-	# Performance_Evaluation_Agent
-    PEA = Researcher("openai/Kimi-K2",tools[1])
-    PET = build_task(prompts[1], expected_outputs[1], PEA)
-
-	# Generate_DAG_Agent
-    GDA = Engineer("openai/Kimi-K2",tools[2])
-    GDT = build_task(prompts[2], expected_outputs[2], PEA)
-
-	# Check_DAG_Agent
-    CDA = Engineer("openai/Kimi-K2",tools[3])
-    CDT = build_task(prompts[3], expected_outputs[3], CDA)
+	# GPT
+    agent = build_agent("openai/Kimi-K2",tools[0])
+    task = build_task(prompts[0], expected_outputs[0], GMA))
 
     # Method_Loop
     method_loop = ReviewLoop(worker=GMA, reviewer=PEA, work_task=GMT, review_task=PET)
